d7d7256eda4e16b40ec8afbbc82924a4bb6cc45f
kyle steinkamp <kyle@hobopanda.com>
2018-05-08 08:00:49 -0500

    Update MTK_CGN_VLAN.sh
    updated interface for fixed name to variable. should update it to be a arg passed from CLI but this works for now.

--- MTK_CGN_VLAN.sh
+++ MTK_CGN_VLAN.sh
@@ -25,6 +25,8 @@ Octet1=100
 Octet2=64
 Octet3=0
 Octet4=0
+interface=sfp-sfpplus2
+interface=ether4
 
 while [ $VLAN -le $endVLAN ]; do
     if [ $VLAN -le "1005" -a  $VLAN -ge "1001" ] || [ $VLAN -le "1" ] || [ $VLAN -gt "4094" ]; then
@@ -42,9 +44,9 @@ while [ $VLAN -le $endVLAN ]; do
     Octet3=$(( $VLAN % 256 )) #to get Octet3 take $VLAN mod 256 this will give the remaining /24 block for Octet3
     Octet2=$((( $VLAN / 256) + 64))   # to get Octet2 tke $VLAN / 256 this will give the /16 block your /24 will sit in + 64 for start block of 64
 
-    echo /interface vlan add interface=sfp-sfpplus2 name=sfpplus2_V$VLAN vlan-id=$VLAN
+    echo /interface vlan add interface=$interface name=$interface"_V"$VLAN vlan-id=$VLAN
     echo /interface bridge add name=BR_V$VLAN protocol-mode=none
-    echo /interface bridge port add bridge=BR_V$VLAN interface=sfpplus2_V$VLAN
+    echo /interface bridge port add bridge=BR_V$VLAN interface=$interface"_V"$VLAN
     echo /ip addres add address=$Octet1.$Octet2.$Octet3.1/24 interface=BR_V$VLAN
     echo /ip pool add name=pool_$VLAN ranges=$Octet1.$Octet2.$Octet3.20-$Octet1.$Octet2.$Octet3.255
     echo /ip dhcp-server network add address=$Octet1.$Octet2.$Octet3.0/24 dns-server=50.30.184.16,66.253.214.16 gateway=$Octet1.$Octet2.$Octet3.1
